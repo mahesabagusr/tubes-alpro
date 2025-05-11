977e90b7255c35578b4bbcf83bf5227fd2062c61
mahesabagusr <mahesabagusraditya1@gmail.com>
2025-05-12 00:45:53 +0700

    feat : Our Partner Section

---
+++ src/constants/OurPartnerList.js
@@ -0,0 +1,38 @@
+import evos from "@/assets/images/evos.jpg";
+import navi from "@/assets/images/navi.png";
+import onic from "@/assets/images/onic.jpg";
+import rrq from "@/assets/images/rrq.png";
+import prx from "@/assets/images/prx.jpg";
+import geek from "@/assets/images/geek.png";
+import liquid from "@/assets/images/liquid.png";
+
+export const partnerLists = [
+  {
+    name: "Evos",
+    logo: evos,
+  },
+  {
+    name: "Navi",
+    logo: navi,
+  },
+  {
+    name: "Onic",
+    logo: onic,
+  },
+  {
+    name: "RRQ",
+    logo: rrq,
+  },
+  {
+    name: "PRX",
+    logo: prx,
+  },
+  {
+    name: "Geek Fam",
+    logo: geek,
+  },
+  {
+    name: "Team Liquid",
+    logo: liquid,
+  },
+];
