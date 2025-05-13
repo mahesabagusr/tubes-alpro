9005dafc05de93b7c466c10606d795b51b5ff380
mahesabagusr <mahesabagusraditya1@gmail.com>
2025-05-13 22:01:10 +0700

    feat : loading screen

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,17 +1,27 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "@/components/NavbarPublic";
 import Footer from "@/components/FooterPublic";
+import LoadingScreen from "@/components/LoadingScreen";
 
 export default function MainLayout() {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
-    <div className="flex flex-col min-h-screen bg-gray-50">
-      <Navbar />
+    <>
+      {isLoading && <LoadingScreen onFinish={() => setIsLoading(false)} />}
 
-      <main className="flex-1 container mx-auto py-4">
-        <Outlet />
-      </main>
-
-      <Footer />
-    </div>
+      <div
+        className={`flex flex-col min-h-screen ${
+          isLoading ? "opacity-0" : "opacity-100"
+        } transition-opacity duration-500`}
+      >
+        <Navbar />
+        <main className="flex-1 container mx-auto py-4">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 }
